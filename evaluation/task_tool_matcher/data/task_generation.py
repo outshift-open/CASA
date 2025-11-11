ad807c1896d877ea847fb7fe569210975c8356b5
melhelou <melhelou@cisco.com>
2025-11-11 13:48:16 +0100

    update task generation

--- evaluation/task_tool_matcher/data/task_generation.py
+++ evaluation/task_tool_matcher/data/task_generation.py
@@ -11,9 +11,9 @@ from typing import List
 from dotenv import dotenv_values
 from openai import AsyncOpenAI
 from pydantic import BaseModel, Field, create_model
+from system_prompts import CONVERSATION_SYSTEM_PROMPT_TEMPLATE as SYSTEM_PROMPT_CONVERSATION
 from system_prompts import OBSCURE_SYSTEM_PROMPT_TEMPLATE as SYSTEM_PROMPT_1_TOOL
 from system_prompts import OBSCURE_SYSTEM_PROMPT_TEMPLATE_N_TOOLS as SYSTEM_PROMPT_N_TOOLS
-from system_prompts import REPHRASE_SYSTEM_PROMPT
 from tqdm import tqdm
 
 # from tqdm.asyncio import tqdm
@@ -42,6 +42,30 @@ def create_tasks_model(n: int) -> type[BaseModel]:
     return tasks_model
 
 
+class SyntheticConversation(BaseModel):
+    """A single synthetic task-oriented conversation leading to a tool call (generated from tools)."""
+
+    conversation_text: str = Field(..., description="The generated conversation text.")
+    number_exchanges: int = Field(
+        ..., description="The total number of exchanges taking place during the conversation."
+    )
+    final_tool_name: str = Field(..., description="The name of the final tool remaining to be called.")
+
+
+def create_conversation_model() -> type[BaseModel]:
+    """Dynamically create a Pydantic model for a synthetic conversation."""
+    conversation_model = create_model(
+        "SyntheticConversation",
+        conversation_text=(str, Field(..., description="The generated conversation text.")),
+        number_exchanges=(
+            int,
+            Field(..., description="The total number of exchanges taking place during the conversation."),
+        ),
+        final_tool_name=(str, Field(..., description="The name of the final tool remaining to be called.")),
+    )
+    return conversation_model
+
+
 async def task_synthesizer(
     tool_set: list[dict], semaphore: asyncio.Semaphore, conversation: bool, n_tasks: int
 ) -> list[dict] | None:
@@ -58,13 +82,12 @@ async def task_synthesizer(
     """
     for tool_data in tool_set:
         tool_description = tool_data["description"]
-        cut_sequence = "\n    Args:\n"  # cutting off input args info (applies to atlassian & hummingbot & paper-search)
-        if cut_sequence in tool_description:
-            tool_description = tool_description.split(cut_sequence)[0]
-        cut_sequence = "\n\nParameters:\n"  # cutting off input args information (applies to nasdaq)
-        if cut_sequence in tool_description:
-            tool_description = tool_description.split(cut_sequence)[0]
-        tool_data["description"] = tool_description
+        # cutting off input args info (applies to atlassian & hummingbot & paper-search, nasdaq, stripe)
+        cut_sequences = ["\n    Args:\n", "\n\nParameters:\n", "\n\nIt takes"]
+        for cut_sequence in cut_sequences:
+            if cut_sequence in tool_description:
+                tool_description = tool_description.split(cut_sequence)[0]
+        tool_data["description"] = tool_description.rstrip("\n")
 
     if len(tool_set) == 1:
         system_prompt_template = SYSTEM_PROMPT_1_TOOL
@@ -82,8 +105,7 @@ async def task_synthesizer(
             ),
         )
 
-    user_prompt = f"Execute the user request and generate {n_tasks} corresponding output examples, make sure the various examples are diverse, **not similar** to each other."
-
+    user_prompt = f"Execute the user request and generate {n_tasks} corresponding output example(s), make sure the various examples are diverse, **not similar** to each other."
     tasks_model = create_tasks_model(n_tasks)
 
     async with semaphore:
@@ -112,18 +134,57 @@ async def task_synthesizer(
             synthetic_tasks = [task.task_text for task in structured_response.tasks]
 
             if conversation:
-                rephrased_tasks = []
+                conversation_samples = []
+                conversation_model = create_conversation_model()
                 for task in synthetic_tasks:
-                    rephrase_prompt = REPHRASE_SYSTEM_PROMPT.format(task=task)
-                    rephrase_response = await client.chat.completions.create(
+                    conversation_prompt = SYSTEM_PROMPT_CONVERSATION.replace("([(User Task Request)])", task)
+                    conversation_prompt = conversation_prompt.replace(
+                        "([(Tools Information)])",
+                        "\n".join(
+                            [
+                                f"**Tool Name:**\n`{tool_data['name']}`\n\n**Tool Description:**\n`{tool_data['description']}`\n\n**Input Schema:**\n`{json.dumps(tool_data['inputSchema'])}`\n\n**Output Schema:**\n`{json.dumps(tool_data['outputSchema'])}`\n\n\n"
+                                for tool_data in tool_set
+                            ]
+                        ),
+                    )
+                    conversation_response = await client.chat.completions.create(
                         model=model_name,
                         messages=[
-                            {"role": "system", "content": rephrase_prompt},
-                            {"role": "user", "content": "Rephrase the above request."},
+                            {"role": "system", "content": conversation_prompt},
+                            {"role": "user", "content": "Execute the above request."},
                         ],
+                        tools=[
+                            {
+                                "type": "function",
+                                "function": {
+                                    "name": "SyntheticConversation",
+                                    "parameters": conversation_model.model_json_schema(),
+                                },
+                            }
+                        ],
+                        tool_choice={"type": "function", "function": {"name": "SyntheticConversation"}},
                     )
-                    rephrased_tasks.append(rephrase_response.choices[0].message.content)
-                synthetic_tasks = rephrased_tasks
+
+                    tool_calls = conversation_response.choices[0].message.tool_calls
+                    if not tool_calls:
+                        print("The conversation model did not return any tool calls.")
+                        return None
+
+                    tasks_json = tool_calls[0].function.arguments
+                    structured_response = conversation_model.model_validate_json(tasks_json)
+
+                    conversation_samples.append(
+                        {
+                            "tool_names": [tool_data["name"] for tool_data in tool_set],
+                            "mcp_servers": [tool_data["mcp_server"] for tool_data in tool_set],
+                            "synthetic_task": structured_response.conversation_text,
+                            "root_intent_task": task,
+                            "number_exchanges": structured_response.number_exchanges,
+                            "final_tool_name": structured_response.final_tool_name,
+                            "system_prompt": "conversation",
+                        }
+                    )
+                return conversation_samples
 
             return [
                 {
@@ -177,17 +238,31 @@ async def process_tools_files(
             if not results:
                 continue
 
-            result = {
-                "tool_names": results[0]["tool_names"],
-                "mcp_servers": results[0]["mcp_servers"],
-                "synthetic_tasks": [res["synthetic_task"] for res in results],
-                "system_prompt": results[0]["system_prompt"]
-                if num_tools == 1
-                else results[0]["system_prompt"] + "_multitool",
-                "tools_per_task": num_tools,
-                "SO_tasks_per_sample": n_tasks,
-                "conversation": True if conversation else False,
-            }
+            if conversation:
+                result = {
+                    "tool_names": results[0]["tool_names"],
+                    "mcp_servers": results[0]["mcp_servers"],
+                    "synthetic_tasks": [res["synthetic_task"] for res in results],
+                    "root_intent_task": [res["root_intent_task"] for res in results],
+                    "number_exchanges": [res["number_exchanges"] for res in results],
+                    "final_tool_name": [res["final_tool_name"] for res in results],
+                    "system_prompt": results[0]["system_prompt"],
+                    "tools_per_task": num_tools,
+                    "SO_tasks_per_sample": n_tasks,
+                    "conversation": True if conversation else False,
+                }
+            else:
+                result = {
+                    "tool_names": results[0]["tool_names"],
+                    "mcp_servers": results[0]["mcp_servers"],
+                    "synthetic_tasks": [res["synthetic_task"] for res in results],
+                    "system_prompt": results[0]["system_prompt"]
+                    if num_tools == 1
+                    else results[0]["system_prompt"] + "_multitool",
+                    "tools_per_task": num_tools,
+                    "SO_tasks_per_sample": n_tasks,
+                    "conversation": True if conversation else False,
+                }
             final_results.append(result)
 
             with open(output_path, "w") as f:
@@ -212,7 +287,7 @@ async def main():
     parser.add_argument(
         "--multiplier", type=int, default=1, help="Number of synthetic tasks to generate per (MCP) tool."
     )
-    parser.add_argument("--conversation", action="store_true", help="Generate conversation tasks if set to True.")
+    parser.add_argument("--conversation", action="store_true", help="Generate conversation data if set to True.")
 
     def positive_int(value):
         ivalue = int(value)
