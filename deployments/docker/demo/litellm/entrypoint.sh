5cb30a3743135ac2fad0f053ffc34494086da1bc
Mohamed Tahar KEDJOUR <mkedjour@cisco.com>
2025-11-27 15:57:42 +0100

    chore(lint): ignored shellcheck useless error
    Signed-off-by: Mohamed Tahar KEDJOUR <mkedjour@cisco.com>

--- deployments/docker/demo/litellm/entrypoint.sh
+++ deployments/docker/demo/litellm/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# shellcheck disable=SC2016 # envsubst requires single quotes not double quotes
 
 envsubst '$OPENAI_GPT4o_API_BASE_URL $OPENAI_GPT4o_API_JWT_TOKEN' < /src/llm/config.env.yaml > /src/llm/config.yaml
 exec "$@"
