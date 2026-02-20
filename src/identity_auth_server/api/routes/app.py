08a1aeb147e874365467e99178778a174115cbc8
Mohamed Tahar KEDJOUR <mkedjour@cisco.com>
2026-02-20 16:36:47 +0100

    chore(app): fixed swagger response model
    Signed-off-by: Mohamed Tahar KEDJOUR <mkedjour@cisco.com>

--- src/identity_auth_server/api/routes/app.py
+++ src/identity_auth_server/api/routes/app.py
@@ -33,7 +33,7 @@ def get_apps(
     return [AppViewModel.model_validate(app) for app in apps]
 
 
-@router.get("/apps/{app_id}", response_model=None)
+@router.get("/apps/{app_id}")
 def get_app(
     app_service: Annotated[AppService, Depends(Container.get_app_service)],
     app_id: str,
