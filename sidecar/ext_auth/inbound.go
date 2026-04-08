210d68580a92457bc4b4ef853ecec73d20a19c9e
Mohamed Tahar KEDJOUR <mkedjour@cisco.com>
2026-04-08 14:32:14 +0200

    feat(ext_auth): added initial inbound logic
    Signed-off-by: Mohamed Tahar KEDJOUR <mkedjour@cisco.com>

--- sidecar/ext_auth/inbound.go
+++ sidecar/ext_auth/inbound.go
@@ -3,25 +3,53 @@ package main
 import (
 	"context"
 	"encoding/hex"
+	"errors"
 	"log/slog"
 
 	authv3 "github.com/envoyproxy/go-control-plane/envoy/service/auth/v3"
 	"google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/grpc/codes"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 )
 
 const (
 	traceParentHeader = "traceparent"
 )
 
-type InboundExtAuthService struct{}
+type InboundExtAuthService struct {
+	k8sDynClient *dynamic.DynamicClient
+	k8sNamespace string
+}
 
-func (s *InboundExtAuthService) Check(_ context.Context, request *authv3.CheckRequest) (*authv3.CheckResponse, error) {
+func NewInboundExtAuthService(
+	k8sDynClient *dynamic.DynamicClient,
+	k8sNamespace string,
+) *InboundExtAuthService {
+	return &InboundExtAuthService{
+		k8sDynClient: k8sDynClient,
+		k8sNamespace: k8sNamespace,
+	}
+}
+
+func (s *InboundExtAuthService) Check(ctx context.Context, request *authv3.CheckRequest) (*authv3.CheckResponse, error) {
+	/*
+		Flow:
+			- get trace id
+			- get service name
+			- fetch its config from the MAS CRD
+			- get the JWT from the backend using the tuple (trace_id, service_name, service_type)
+			- does it have a JWT?
+			- yes -> validate it based on the service type (agent, mcp)
+			- if invalid return 403
+			- if MCP without JWT -> return 403
+			- if it's an agent/client without JWT
+				- take the prompt and create a mapping between the trace ID and the prompt (user_input_id)
+				- call /token and store the token
+	*/
+
 	attrs := request.GetAttributes()
 
 	httpReq := attrs.GetRequest().GetHttp()
@@ -34,58 +62,44 @@ func (s *InboundExtAuthService) Check(_ context.Context, request *authv3.CheckRe
 			return nil, err
 		}
 
+		// get the trace ID
 		traceID := hex.EncodeToString(tp.TraceID[:])
 		slog.Info("", "traceID", traceID)
 
-		// host := httpReq.Host
+		// get the service name
+		host := httpReq.Host
+		slog.Info("", "serviceName", host)
 
-		config, err := rest.InClusterConfig()
-		if err != nil {
-			slog.Error("Error create K8S InCluster config", "err", err)
-			return nil, err
-		}
-
-		clientset, err := kubernetes.NewForConfig(config)
-		if err != nil {
-			slog.Error("Error create K8S client", "err", err)
-			return nil, err
-		}
-
-		_, err = clientset.CoreV1().Pods("zta-sidecar").List(context.Background(), metav1.ListOptions{})
-		if err != nil {
-			slog.Error("Error getting PODs", "err", err)
-			return nil, err
-		}
-
-		// slog.Info("Fetched PODs", "pods", pods)
-
-		dynClient, err := dynamic.NewForConfig(config)
-		if err != nil {
-			slog.Error("Error create K8S dynamic client", "err", err)
-			return nil, err
-		}
-
+		// fetch the list of MAS CRs
 		gvr := schema.GroupVersionResource{
 			Group:    "zta.io",
 			Version:  "v1",
 			Resource: "multiagentsystems",
 		}
 
-		obj, err := dynClient.Resource(gvr).Namespace("zta-sidecar").List(context.Background(), metav1.ListOptions{})
+		resource, err := s.k8sDynClient.
+			Resource(gvr).
+			Namespace(s.k8sNamespace).
+			List(ctx, metav1.ListOptions{})
 		if err != nil {
 			slog.Error("Error listing MAS CRDs", "err", err)
 			return nil, err
 		}
 
-		for _, item := range obj.Items {
-			slog.Info("MAS CRD", "name", item.GetName(), "crd", item)
+		if len(resource.Items) == 0 {
+			return nil, errors.New("no MultiAgentSystem resource found")
 		}
 
-		// what do i need?
-		// - traceID
-		// - in service
-		// - out service
-		// - if client service then input schema to parse the prompt
+		// get the first one in the list
+		masDef := resource.Items[0]
+
+		apps, _, _ := unstructured.NestedSlice(masDef.Object, "spec", "apps")
+		for _, app := range apps {
+			m, ok := app.(map[string]any)
+			if ok {
+				slog.Info("Got app", "baseUrl", m["baseUrl"], "name", m["name"], "type", m["type"])
+			}
+		}
 	}
 
 	return &authv3.CheckResponse{
--- sidecar/ext_auth/main.go
+++ sidecar/ext_auth/main.go
@@ -6,6 +6,9 @@ import (
 	"log/slog"
 	"os"
 	"os/signal"
+
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
 )
 
 const (
@@ -17,7 +20,16 @@ func main() {
 	ctx := context.Background()
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 
-	inboundExtAuthServer, err := NewExtAuthServer(inboundExtAuthHost, &InboundExtAuthService{})
+	k8sDynClient, err := createKubernetesDyncClient()
+	if err != nil {
+		logger.Error("Failed to create Kubernetes dynamic client", "err", err)
+		os.Exit(-1)
+	}
+
+	inboundExtAuthServer, err := NewExtAuthServer(
+		inboundExtAuthHost,
+		NewInboundExtAuthService(k8sDynClient, "zta-sidecar"),
+	)
 	if err != nil {
 		logger.Error("Failed to create the HTTP inbound ext auth server", slog.Any("err", err))
 		os.Exit(-1)
@@ -60,4 +72,32 @@ func main() {
 	<-interrupChannel
 
 	logger.Info("Exiting the ext auth service")
+}
+
+func createKubernetesDyncClient() (*dynamic.DynamicClient, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, fmt.Errorf("error create K8S InCluster config: %w", err)
+	}
+
+	// clientset, err := kubernetes.NewForConfig(config)
+	// if err != nil {
+	// 	slog.Error("Error create K8S client", "err", err)
+	// 	return nil, err
+	// }
+
+	// _, err = clientset.CoreV1().Pods("zta-sidecar").List(context.Background(), metav1.ListOptions{})
+	// if err != nil {
+	// 	slog.Error("Error getting PODs", "err", err)
+	// 	return nil, err
+	// }
+
+	// slog.Info("Fetched PODs", "pods", pods)
+
+	dynClient, err := dynamic.NewForConfig(config)
+	if err != nil {
+		return nil, fmt.Errorf("error create K8S dynamic client: %w", err)
+	}
+
+	return dynClient, nil
 }
