71daee982896b307ca5d3152808cb17f6f4bad8a
jdiaconu <jdiaconu@cisco.com>
2026-04-14 16:42:41 +0200

    feat: Cleanup
    Signed-off-by: jdiaconu <jdiaconu@cisco.com>

--- deployments/docker/demo/litellm/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# shellcheck disable=SC2016 # envsubst requires single quotes not double quotes
-
-envsubst '$OPENAI_GPT4o_MODEL_ID $OPENAI_GPT4o_API_BASE_URL $OPENAI_GPT4o_API_JWT_TOKEN' < /src/llm/config.env.yaml > /src/llm/config.yaml
-exec "$@"
